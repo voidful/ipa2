b400218f1b7e5f6713060a8e99aee41ab08a2381
Eric Chung <lam970805@gmail.com>
2019-07-25 00:45:15 +0800

    add init.py

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import unittest
 from main import *
-
+from ipa2 import IPAConverter
 
 class Test(unittest.TestCase):
 
     def test_IPA(self):
+        ipa = IPAConverter('yue')
+        self.assertEqual(['nei˩˧ hou˧˥', 'lei˩˧ hou˧˥'], ipa.convert_sent("你好"))
+
+    def test_pip(self):
         ipa = IPAConverter('yue')
         self.assertEqual(['nei˩˧ hou˧˥', 'lei˩˧ hou˧˥'], ipa.convert_sent("你好"))
